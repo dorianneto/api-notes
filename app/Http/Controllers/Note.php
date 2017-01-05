28a3e33115b109fdd8c9637e0050edeb180888b4
Dorian Neto <doriansampaioneto@gmail.com>
2017-01-05 18:21:44 -0300

    feat(note): CRUD implemented

--- app/Http/Controllers/Note.php
+++ app/Http/Controllers/Note.php
@@ -2,12 +2,88 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use App\Note as Model;
 use JWTAuth;
+use Validator;
 
 class Note extends Controller
 {
-    public function get()
+    protected $model;
+
+    public function __construct(Model $model)
     {
-        return JWTAuth::user();
+        $this->model = $model;
+    }
+
+    public function get(Request $request)
+    {
+        $all = $this->model->where('user_id', JWTAuth::user()->id)->get();
+
+        return response()->json($all);
+    }
+
+    public function find(Request $request, $id)
+    {
+        $all = $this->model->where('user_id', JWTAuth::user()->id)->find($id);
+
+        return response()->json($all);
+    }
+
+    public function store(Request $request)
+    {
+        $request->merge(['user_id' => JWTAuth::user()->id]);
+
+        $data      = $request->all();
+        $validator = Validator::make($data, [
+            'text' => 'required|max:255'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
+        $stored = $this->model->insert($data);
+
+        return response()->json(['message' => 'Note stored!']);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->merge(['user_id' => JWTAuth::user()->id]);
+
+        $data      = $request->all();
+        $validator = Validator::make($data, [
+            'text' => 'required|max:255'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
+        $updated = $this->model
+            ->where('id', $id)
+            ->where('user_id', JWTAuth::user()->id)
+            ->update($data);
+
+        if ($updated == 0) {
+            return response()->json(['message' => 'ID not found.'], 400);
+        }
+
+        return response()->json(['message' => 'Note updated!']);
+    }
+
+    public function destroy(Request $request, $id)
+    {
+        $deleted = $this->model
+            ->where('id', $id)
+            ->where('user_id', JWTAuth::user()->id)
+            ->delete();
+
+        if ($deleted == 0) {
+            return response()->json(['message' => 'ID not found.'], 400);
+        }
+
+        return response()->json(['message' => 'Note deleted!']);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,22 +1,14 @@
 <?php
 
-// no middleware de auth, vou verificar o token
 $app->group(['prefix' => 'api', 'middleware' => 'auth'], function () use ($app) {
     $app->get('notes', 'Note@get');
-    // $app->get('notes/{id}', 'NotesController@find');
-    // $app->post('notes', 'NotesController@store');
-    // $app->put('notes/{id}', 'NotesController@update');
-    // $app->delete('notes/{id}', 'NotesController@destroy');
+    $app->get('notes/{id}', 'Note@find');
+    $app->post('notes', 'Note@store');
+    $app->put('notes/{id}', 'Note@update');
+    $app->delete('notes/{id}', 'Note@destroy');
 });
 
-// logar usando email e pass para resgatar o token
 $app->post('auth', 'Auth\LoginController@authenticate');
-
-// resetar email e pass através do envio de email
 // $app->post('reset', 'Auth\AuthController@reset');
-
-// limpar sessão e excluir token
 $app->post('logout', 'Auth\LoginController@logout');
-
-// cadastrar usuário sem o token
 // $app->post('sign_up', 'Auth\RegisterController@register');
