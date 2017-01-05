75de35fd06c3e9de6fc8620162767030e8d251ea
Dorian Neto <doriansampaioneto@gmail.com>
2017-01-05 09:48:30 -0300

    feat(migrations): migrations to create users, notes and password resets table generated

---
+++ database/migrations/2017_01_05_121356_create_notes_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->string('text');
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+
+            // Indexes
+            $table->index('user_id');
+
+            // Foreign keys
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notes');
+    }
+}
