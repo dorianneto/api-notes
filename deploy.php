e63aed62b18e7d5db8ab4e777e48b1514aa2fe8c
Dorian Neto <doriansampaioneto@gmail.com>
2017-01-05 00:01:39 -0300

    feat(deployer): deploy file added

---
+++ deploy.php
@@ -0,0 +1,53 @@
+<?php
+require 'recipe/laravel.php';
+
+// Set configurations
+set('repository', 'git@github.com:dorianneto/api-notes.git');
+set('writable_dirs', ['bootstrap', 'storage']);
+set('default_stage', 'production');
+env('composer_options', 'install --verbose --prefer-dist --optimize-autoloader --no-progress --no-interaction');
+
+// Configure servers
+server('prod', '104.131.82.121')
+    ->user('root')
+    ->identityFile()
+    ->stage('production')
+    ->env('branch', 'master')
+    ->env('deploy_path', '/var/www/html/dorianneto.com.br/hsa/production');
+
+// Tasks
+task('deploy:permissions', function () {
+    cd('{{release_path}}');
+    run('sudo chown -R www-data:www-data {{release_path}}');
+    run('sudo chmod -R 775 bootstrap/');
+    run('sudo chmod -R 775 storage/');
+})->desc('Set bootstrap path permissions');
+
+task('artisan:migrate', function () {
+    $output = run('{{bin/php}} {{release_path}}/artisan migrate --force');
+    writeln('<info>' . $output . '</info>');
+})->desc('Execute artisan migrations');
+
+task('deploy:configure', function () {
+    env('timezone', 'America/Fortaleza');
+})->desc('Execute deploy configuration');
+
+task('env_file', function() {
+    upload(__DIR__ . DIRECTORY_SEPARATOR . '.env.prod', '{{deploy_path}}/current/.env');
+});
+
+task('deploy', [
+    'deploy:configure',
+    'deploy:prepare',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:symlink',
+    'deploy:writable',
+    'deploy:permissions',
+    'deploy:shared',
+    'deploy:vendors',
+    'cleanup',
+    'env_file',
+    'artisan:cache:clear',
+    'artisan:migrate'
+]);
