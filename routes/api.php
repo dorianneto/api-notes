cb22e31fdd28a0502f93387eb66f53e74763095d
Dorian Neto <doriansampaioneto@gmail.com>
2017-01-06 10:17:36 -0300

    feat(routes): included auth routes into api group

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,18 @@
 <?php
 
-$app->group(['prefix' => 'api', 'middleware' => 'auth'], function () use ($app) {
-    $app->get('notes', 'Note@get');
-    $app->get('notes/{id}', 'Note@find');
-    $app->post('notes', 'Note@store');
-    $app->put('notes/{id}', 'Note@update');
-    $app->delete('notes/{id}', 'Note@destroy');
-});
+$app->group(['prefix' => 'api'], function () use ($app) {
+    $app->post('auth', 'Auth\Login@authenticate');
+    $app->post('reset', 'Auth\Password@send');
+    $app->put('reset/{token}', 'Auth\Password@reset');
+    $app->post('sign_up', 'User@store');
 
-$app->post('auth', 'Auth\Login@authenticate');
-$app->post('reset', 'Auth\Password@send');
-$app->put('reset/{token}', 'Auth\Password@reset');
-$app->post('logout', 'Auth\Login@logout');
-$app->post('sign_up', 'User@store');
+    $app->group(['middleware' => 'auth'], function() use ($app) {
+        $app->post('logout', 'Auth\Login@logout');
+
+        $app->get('notes', 'Note@get');
+        $app->get('notes/{id}', 'Note@find');
+        $app->post('notes', 'Note@store');
+        $app->put('notes/{id}', 'Note@update');
+        $app->delete('notes/{id}', 'Note@destroy');
+    });
+});
