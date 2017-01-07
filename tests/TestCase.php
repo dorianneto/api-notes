40ef7900682671543d1458fbfdb6846bc8259bb7
Dorian Neto <doriansampaioneto@gmail.com>
2017-01-07 02:43:40 -0300

    fix(travis-ci): database settings

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,8 @@
 
 class TestCase extends Laravel\Lumen\Testing\TestCase
 {
+    const BASE_URL = 'http://hsa.dorianneto.com.br/';
+
     const EMAIL = 'test@test.com';
 
     const PASSWORD = '3z5DSDPP';
@@ -18,8 +20,8 @@ class TestCase extends Laravel\Lumen\Testing\TestCase
 
     protected function getToken()
     {
-        $response = $this->call('POST', '/api/auth', ['email' => self::EMAIL, 'password' => self::PASSWORD]);
-        $token    = $response->getData()->token;
+        $response = $this->post('api/auth', ['email' => self::EMAIL, 'password' => self::PASSWORD]);
+        $token    = json_decode($response->response->getContent())->token;
 
         return $token;
     }
